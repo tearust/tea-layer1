b61c83121476307fe33336b2f381d44e41e2d8a1
jacky.li <jacky.li@accenture.com>
2021-04-08 03:37:39 +0800

    update

--- run-local.sh
+++ run-local.sh
@@ -1,3 +1,6 @@
 
-./target/debug/tea-layer1 --dev --alice --tmp --port 30334 --ws-port 9944 --rpc-port 9933
-# ./target/debug/tea-layer1 --dev --alice --port 30334 --ws-port 9944 --rpc-port 9933
+# ./target/debug/tea-layer1 --dev --alice --tmp --port 30334 --ws-port 9944 --rpc-port 9933
+
+
+./target/debug/tea-layer1 purge-chain --dev
+./target/debug/tea-layer1 --dev --alice --port 30334 --ws-port 9944 --rpc-port 9933
